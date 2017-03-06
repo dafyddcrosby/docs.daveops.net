528df69036ccf3b914856f9572691b26e1ebc888
David Crosby <dave@dafyddcrosby.com>
2017-03-05 20:07:02 -0700

    Remove www.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://www.daveops.net'
+SITEURL = 'https://daveops.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
