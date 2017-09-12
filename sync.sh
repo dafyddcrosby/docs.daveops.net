b2186d64bb98b627863200f0fa68e0fc4e824b78
David Crosby <dave@dafyddcrosby.com>
2017-09-12 13:16:47 -0600

    sync

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 rm -rf html
 zim --export --index-page index -r --template ZeroFiveEight -o html --overwrite ./
-find . -name '*.html' -exec gzip -k {} \;
+#find . -name '*.html' -exec gzip -k {} \;
 cp .htaccess html/
 rsync -av --delete html/ daveops:daveops.net/
